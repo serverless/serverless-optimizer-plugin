ba4693db749739d76d85e52ff4911dbc0299dfd4
ac360 <austen@serverless.com>
2016-01-17 13:37:19 -0800

    Update for Serverless v0.1.0

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 Serverless Optimizer Plugin
 =============================
 
-**Note:** Requires Serverless *v0.0.13* or higher.
+Browserifies, minifies your Serverless Node.js Functions on deployment, and more!
 
-Currently a work in progress :)  Needs to print debug information but there is currently a bug there.
+Reducing the file size of your AWS Lambda Functions allows AWS to provision them more quickly, speeding up the response time of your Lambdas.  Smaller Lambda sizes also helps you develop faster because you can upload them faster.  This Severless Plugin is absolutely recommended for every project including Lambdas with Node.js.
 
-Looks for the following properties on your Functions in `s-function.json`:
+**Note:** Requires Serverless *v0.1.0* or higher.
+
+###Setup
+
+* Install via npm in the root of your Serverless Project:
+```
+npm install serverless-optimizer-plugin --save
+```
+
+* In the `custom` property of either your `s-component.json` or `s-function.json` add an optimize property.
 
 ```
 "custom": {
-	"optimize": {
-		"browserify": {
-			"babelify": true
-		},
-		"minify": true
-	}
+	"optimize": true
 }
 ```
 
+* Adding the `custom.optimize` property in `s-component.json` applies the optimization setting to ALL functions in that component.  Adding the `custom.optimize` property to `s-function.json` applies the optimization setting to ONLY that specific function.  You can use `custom.optimize` in both places.  The `custom.optimize` setting in `s-function.json` will override the setting in `s-component.json`.
+
+We're currently working on adding support for Babelify and Typsecript.  Check back for updates!
--- index.js
+++ index.js
@@ -6,14 +6,15 @@
 
 module.exports = function(ServerlessPlugin) {
 
-  const path       = require('path'),
-      fs           = require('fs'),
-      os           = require('os'),
-      babelify     = require('babelify'),
-      browserify   = require('browserify'),
-      UglifyJS     = require('uglify-js'),
-      wrench       = require('wrench'),
-      BbPromise    = require('bluebird');
+  const path    = require('path'),
+    _           = require('lodash'),
+    fs          = require('fs'),
+    os          = require('os'),
+    babelify    = require('babelify'),
+    browserify  = require('browserify'),
+    UglifyJS    = require('uglify-js'),
+    wrench      = require('wrench'),
+    BbPromise   = require('bluebird');
 
   /**
    * ServerlessOptimizer
@@ -25,8 +26,8 @@ module.exports = function(ServerlessPlugin) {
      * Constructor
      */
 
-    constructor(S, config) {
-      super(S, config);
+    constructor(S) {
+      super(S);
     }
 
     /**
@@ -45,10 +46,10 @@ module.exports = function(ServerlessPlugin) {
 
       this.S.addHook(this._optimize.bind(this), {
         action: 'codePackageLambdaNodejs',
-        event:  'post'
+        event: 'post'
       });
 
-      return Promise.resolve();
+      return BbPromise.resolve();
     }
 
     /**
@@ -57,53 +58,96 @@ module.exports = function(ServerlessPlugin) {
 
     _optimize(evt) {
 
-      let _this = this;
+      // Validate: Check Serverless version
+      if (parseInt(this.S._version.split('.')[1]) < 1) {
+        console.log("WARNING: This version of the Serverless Optimizer Plugin will not work with a version of Serverless that is less than v0.1.");
+      }
 
-      // Skip plugin if this function is not optimized
-      if (!evt.function.custom.optimize) return BbPromise.resolve(evt);
+      // Get function
+      let func    = this.S.state.getFunctions({  paths: [evt.options.path] })[0],
+        component = this.S.state.getComponents({ component: func._config.component })[0],
+        optimizer;
 
-      if (evt.function.custom.optimize.browserify) {
+      // Skip if no optimization is set on component OR function
+      if ((!component.custom || !component.custom.optimize) && (!func.custom || !func.custom.optimize)) {
+        return BbPromise.resolve(evt);
+      }
 
-        return _this._browserifyBundle(evt)
-            .then(optimizedCodeBuffer => {
+      // If optimize is set in component, but false in function, skip
+      if (component.custom && component.custom.optimize && func.custom && func.custom.optimize === false) {
+        return BbPromise.resolve(evt);
+      }
 
-              let envData         = fs.readFileSync(path.join(evt.function.pathDist, '.env')),
-                  handlerFileName = evt.function.handler.split('.')[0];
-
-              // Create pathsPackaged for each file ready to compress
-              evt.function.pathsPackaged   = [
-                // handlerFileName is the full path lambda file including dir rel to back
-                { fileName: handlerFileName + '.js', data: optimizedCodeBuffer },
-                { fileName: '.env', data: envData },
-              ];
-
-              evt.function.pathsPackaged = evt.function.pathsPackaged.concat(_this._generateIncludePaths(evt));
-
-              return evt;
-            })
-            .catch(function(e) {console.log(e)});
+      // Optimize: Nodejs
+      if (func.runtime === 'nodejs') {
+        optimizer = new OptimizeNodejs(this.S, evt, component, func);
+        return optimizer.optimize()
+          .then(function(evt) {
+            return evt;
+          });
       }
 
       // Otherwise, skip plugin
-      return BbPromise.resolve(evt)
+      return BbPromise.resolve(evt);
+    }
+  }
+
+  /**
+   * Optimize Nodejs
+   * - Separate class allows this Hook to be run concurrently safely.
+   */
+
+  class OptimizeNodejs {
+
+    constructor(S, evt, component, func) {
+      this.S          = S;
+      this.evt        = evt;
+      this.component  = component;
+      this.function   = func;
+    }
+
+    optimize() {
+
+      let _this = this;
+
+      _this.config = {
+        handlerExt:   'js',
+        includePaths: [],
+        requires:     [],
+        plugins:      [],
+        transforms:   [],
+        exclude:      [],
+        ignore:       []
+      };
+      _this.config = _.merge(
+        _this.config,
+        _this.component.custom.optimize ? _this.component.custom.optimize === true ? {} : _this.component.custom.optimize : {},
+        _this.function.custom.optimize ? _this.function.custom.optimize === true ? {} : _this.function.custom.optimize : {}
+      );
+
+      // Browserify
+      return _this.browserify()
+        .then(function() {
+          return _this.evt;
+        });
     }
 
     /**
-     * Browserify Bundle
-     * - Browserify the code and return buffer of bundled code
+     * Browserify
+     * - Options: babelify, transform, exclude, minify, ignore
      */
 
-    _browserifyBundle(evt) {
+    browserify() {
 
       let _this       = this;
       let uglyOptions = {
         mangle:   true, // @see http://lisperator.net/uglifyjs/compress
-        compress: {},
+        compress: {}
       };
 
       let b = browserify({
-        basedir:          evt.function.pathDist,
-        entries:          [evt.function.handler.split('.')[0] + '.js'],
+        basedir:          fs.realpathSync(_this.evt.data.pathDist),
+        entries:          [_this.function.handler.split('.')[0] + '.' + _this.config.handlerExt],
         standalone:       'lambda',
         browserField:     false,  // Setup for node app (copy logic of --node in bin/args.js)
         builtins:         false,
@@ -113,118 +157,146 @@ module.exports = function(ServerlessPlugin) {
         insertGlobalVars: {      // Handle process https://github.com/substack/node-browserify/issues/1277
           //__filename: insertGlobals.lets.__filename,
           //__dirname: insertGlobals.lets.__dirname,
-          process: function() {}
+          process: function () {
+          }
         }
       });
 
-      // If Babelify is set, do it here
-      if (evt.function.custom.optimize.babelify) b.transform(babelify);
+      // browserify.require
+      _this.config.requires.map(req => {
+        if (typeof(req) === typeof('')) req = {name: req};
+        b.require(req.name, req.opts);
+      });
 
-      // Add custom transforms
-      if (evt.function.custom.optimize.transform) {
-        //SUtils.sDebug(`"${evt.stage} - ${evt.region.region} - ${evt.function.name}": Adding transform - ${evt.function.package.optimize.transform}`);
-        b.transform(evt.function.custom.optimize.transform);
-      }
+      // browserify.plugin
+      _this.config.plugins.map(plug => {
+        if (typeof(plug) === typeof('')) plug = {name: plug};
+        b.plugin(require(plug.name), plug.opts);
+      });
+
+      // browserify.transform
+      _this.config.transforms.map(transform => {
+        if (typeof(transform) === typeof('')) transform = {name: transform};
+        b.transform(require(transform.name), transform.opts);
+      });
 
       // browserify.exclude
-      if (evt.function.custom.optimize.browserify.exclude) {
-        evt.function.custom.optimize.browserify.exclude.forEach(file => {
-          b.exclude(file);
-        });
-      }
+      _this.config.exclude.forEach(file => b.exclude(file));
 
       // browserify.ignore
-      if (evt.function.custom.optimize.browserify.ignore) {
-        evt.function.custom.optimize.browserify.ignore.forEach(file => {
-          b.ignore(file);
-        });
-      }
+      _this.config.ignore.forEach(file => b.ignore(file));
 
       // Perform Bundle
-      let bundledFilePath = path.join(evt.function.pathDist, 'bundled.js');   // Save for auditing
-      let minifiedFilePath = path.join(evt.function.pathDist, 'minified.js'); // Save for auditing
+      _this.pathBundled   = path.join(_this.evt.data.pathDist, 'bundled.js');   // Save for auditing
+      _this.pathMinified  = path.join(_this.evt.data.pathDist, 'minified.js');  // Save for auditing
 
-      return new BbPromise(function(resolve, reject) {
+      return new BbPromise(function (resolve, reject) {
 
-        b.bundle(function(err, bundledBuf) {
+        b.bundle(function (err, bundledBuf) {
 
           if (err) {
             console.error('Error running browserify bundle');
             reject(err);
           } else {
 
-            fs.writeFileSync(bundledFilePath, bundledBuf);
-            //SUtils.sDebug(`"${evt.stage} - ${evt.region.region} - ${evt.function.name}": Bundled file created - ${bundledFilePath}`);
+            fs.writeFileSync(_this.pathBundled, bundledBuf);
 
-            if (evt.function.custom.optimize.minify) {
+            // Minify browserified data
 
-              //SUtils.sDebug(`"${evt.stage} - ${evt.region.region} - ${evt.function.name}": Minifying...`);
+            if (_this.function.custom.optimize && _this.function.custom.optimize.minify !== false) {
 
-              let result = UglifyJS.minify(bundledFilePath, uglyOptions);
+              let result = UglifyJS.minify(_this.pathBundled, uglyOptions);
 
-              if (!result || !result.code) {
-                reject(new SError('Problem uglifying code'));
-              }
+              if (!result || !result.code) return reject(new SError('Problem uglifying code'));
 
-              fs.writeFileSync(minifiedFilePath, result.code);
+              fs.writeFileSync(_this.pathMinified, result.code);
 
-              //SUtils.sDebug(`"${evt.stage} - ${evt.region.region} - ${evt.function.name}": Minified file created - ${minifiedFilePath}`);
-
-              resolve(result.code);
+              resolve(_this.pathMinified);
             } else {
-              resolve(bundledBuf);
+              resolve(_this.pathBundled);
             }
           }
         });
-      });
+      })
+        .then(pathOptimized => {
+
+          // Save final optimized path
+          _this.pathOptimized = pathOptimized;
+
+          let envData       = fs.readFileSync(path.join(_this.evt.data.pathDist, '.env')),
+            handlerFileName = _this.function.handler.split('.')[0];
+
+          // Reassign pathsPackages property
+          _this.evt.data.pathsPackaged = [
+            {
+              name: handlerFileName + '.js',
+              path: _this.pathOptimized
+            },
+            {
+              name: '.env',
+              path: path.join(_this.evt.data.pathDist, '.env')
+            }
+          ];
+
+          // Reassign pathsPackages property
+          _this.evt.data.pathsPackaged = _this.evt.data.pathsPackaged.concat(_this._generateIncludePaths());
+        });
     }
 
     /**
      * Generate Include Paths
+     * - If function.custom.includePaths are specified, include them
      */
 
-    _generateIncludePaths(evt) {
+    _generateIncludePaths() {
 
-      let compressPaths = [],
-          ignore        = ['.DS_Store'],
-          stats,
-          fullPath;
+      let _this       = this,
+        compressPaths = [],
+        ignore        = ['.DS_Store'],
+        stats,
+        fullPath;
 
       // Skip if undefined
-      if (!evt.function.custom.optimize.includePaths) return compressPaths;
+      if (!_this.function.custom.includePaths) return compressPaths;
 
       // Collect includePaths
-      evt.function.custom.optimize.includePaths.forEach(p => {
+      _this.function.custom.includePaths.forEach(p => {
 
         try {
-          fullPath = path.resolve(path.join(evt.function.pathDist, p));
-          stats    = fs.lstatSync(fullPath);
+          fullPath = path.resolve(path.join(_this.evt.data.pathDist, p));
+          stats = fs.lstatSync(fullPath);
         } catch (e) {
           console.error('Cant find includePath ', p, e);
           throw e;
         }
 
         if (stats.isFile()) {
-          compressPaths.push({fileName: p, data: fs.readFileSync(fullPath)});
+          compressPaths.push({
+            name: p,
+            path: fullPath
+          });
         } else if (stats.isDirectory()) {
 
           let dirname = path.basename(p);
 
           wrench
-              .readdirSyncRecursive(fullPath)
-              .forEach(file => {
+            .readdirSyncRecursive(fullPath)
+            .forEach(file => {
 
-                // Ignore certain files
-                for (let i = 0; i < ignore.length; i++) {
-                  if (file.toLowerCase().indexOf(ignore[i]) > -1) return;
-                }
+              // Ignore certain files
+              for (let i = 0; i < ignore.length; i++) {
+                if (file.toLowerCase().indexOf(ignore[i]) > -1) return;
+              }
 
-                let filePath = [fullPath, file].join('/');
-                if (fs.lstatSync(filePath).isFile()) {
-                  let pathInZip = path.join(dirname, file);
-                  compressPaths.push({fileName: pathInZip, data: fs.readFileSync(filePath)});
-                }
-              });
+              let filePath = [fullPath, file].join('/');
+              if (fs.lstatSync(filePath).isFile()) {
+                let pathInZip = path.join(dirname, file);
+                compressPaths.push({
+                  name: pathInZip,
+                  path: filePath
+                });
+              }
+            });
         }
       });
 
@@ -232,7 +304,5 @@ module.exports = function(ServerlessPlugin) {
     }
   }
 
-  // Export Plugin Class
   return ServerlessOptimizer;
-
 };
